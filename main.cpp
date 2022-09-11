a320c84f8092b245ac7a6d68b0be99173e6e6eba
zsxlti <zsoltibaloghvp@gmail.com>
2022-09-11 20:44:39 +0200

    hibak kommentezese

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //konstans valtozo neve hibas
+    std::cout << '1-100 ertekek duplazasa' //hianyza sorvegjel, szoveg nem dupla idezojelek kozt talalhato, soremeles hianya
+    for (int i = 0;) //hianyos ciklusfej
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //hianyzo dereferencia
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hianyos ciklusfej
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hianyzo sorvegjel
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //int helyett double hasznalata, tizedesjegy levagas elkerulese vegett
+    for (int i = 0; i < N_ELEMENTS, i++) //','-t ';'-re kell cserelni
     {
-        atlag += b[i]
+        atlag += b[i] //hianyzo sorvegjel, hianyzo dereferencia
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //hianyzo destruktor
     return 0;
 }
