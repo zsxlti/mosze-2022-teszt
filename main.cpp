be58380b40267e9bd205e5b4c78f39a8086ab9b1
zsxlti <zsoltibaloghvp@gmail.com>
2022-09-12 19:31:07 +0200

    100 ketszerese javitva

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ int main()
 {
     int *b = new int[N_ELEMENTS]; //konstans valtozo neve hibas
     std::cout << "1-100 ertekek duplazasa" << std::endl; //hianyzo sorvegjel, szoveg nem dupla idezojelek kozt talalhato, soremeles szebbe esztetikusabba teszi a kiirast
-    for (int i = 1; i < N_ELEMENTS; i++) //hianyos ciklusfeltetel, i-nek 1-nek kell lennie
+    for (int i = 1; i <= N_ELEMENTS; i++) //hianyos ciklusfeltetel, i-nek 1-nek kell lennie
     {
         b[i] = i * 2;
     }
-    for (int i = 1; i< N_ELEMENTS; i++) //hianyos ciklusfeltetel
+    for (int i = 1; i<= N_ELEMENTS; i++) //hianyos ciklusfeltetel
     {
         std::cout << "Ertek:"; //hianyzo sorvegjel, ertek kiirasa hianyzik
         std::cout << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     double atlag; //int helyett double hasznalata, tizedesjegy levagas elkerulese vegett
-    for (int i = 1; i < N_ELEMENTS; i++) //','-t ';'-re kell cserelni, ciklusvaltozo minden esetben 1-tol kell, hogy induljon
+    for (int i = 1; i <= N_ELEMENTS; i++) //','-t ';'-re kell cserelni, ciklusvaltozo minden esetben 1-tol kell, hogy induljon
     {
         atlag += b[i]; //hianyzo sorvegjel
     }
